d0afe338cb058d06cecd83acd8ef7f73931b29e4
ruslanzalikov <rus.zalikov@bk.ru>
2021-11-29 18:50:22 +0300

    second commit

--- app.py
+++ app.py
@@ -13,13 +13,14 @@ cursor = conn.cursor()
 
 @app.route('/login/', methods=['GET'])
 def index():
-   return render_template('login.html')
+    return render_template('login.html')
 
 
 @app.route('/login/', methods=['POST'])
 def login():
-   username = request.form.get('username')
-   password = request.form.get('password')
-   cursor.execute("SELECT * FROM service.users WHERE login=%s AND password=%s", (str(username), str(password)))
-   records = list(cursor.fetchall())
-   return render_template('account.html', full_name=records[0][1])
+    username = request.form.get('username')
+    password = request.form.get('password')
+    cursor.execute("SELECT * FROM service.users WHERE login=%s AND password=%s", (str(username), str(password)))
+    records = list(cursor.fetchall())
+    if username=True and password=True:
+        return render_template('account.html', full_name=records[0][1], username=username, password=password)
